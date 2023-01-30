527a713abe789446436bafd5e4bba984b64a1868
RODRIGO REIS PRADO <rodrigo.prado@viavarejo.com.br>
2023-01-30 10:14:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia = new Nokia
+    (
+        "51995858877",
+        "Aeros", 
+        "000111", 
+        128
+    );
+    
+Smartphone iphone = new Iphone
+    (
+        "51981105178",
+        "Mac", 
+        "11000", 
+        256
+    );
+    
+Console.WriteLine(nokia);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Facebook");
+nokia.ReceberLigacao();
+
+Console.WriteLine(iphone);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.InstalarAplicativo("Facebook");
+iphone.ReceberLigacao();
